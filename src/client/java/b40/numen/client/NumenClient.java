5b98c94a2dd4c1bb117a98e9799b8a05fd6ac515
BaoBao-creator <ba0ba0113377@gmail.com>
2026-06-23 19:52:09 +0700

    Initial commit - Setup Numen mod structure

---
+++ src/client/java/b40/numen/client/NumenClient.java
@@ -0,0 +1,10 @@
+package b40.numen.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NumenClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
